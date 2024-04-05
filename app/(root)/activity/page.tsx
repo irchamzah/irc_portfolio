1cde731402f7f0cfb29cf5b3bd8bbd5264113739
irchamzah <71950891+irchamzah@users.noreply.github.com>
2024-04-05 13:13:46 +0700

    Update page.tsx

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -6,7 +6,7 @@ import { redirect } from "next/navigation";
 
 async function Page() {
   const user = await currentUser();
-  // if (!user) return null;
+  if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
